f16a9d27820691acc0e783797504c387f1da385a
Bharat Ayyar <bayyar@gmail.com>
2015-08-27 09:34:43 -0400

    Better jQuery around button interactions

--- bb.js
+++ bb.js
@@ -57,57 +57,71 @@ $.getJSON(url, function(data) {
 
 var picks = 0, curValue = 0, voa = 0;
 var numRb = 0, numWr = 0, numTe = 0, numQb = 0;
-var avgValue = {0:0,1:6.5,2:25,3:55.5,4:98,5:152.5,6:219,7:297.5,8:388,9:490.5,10:605,11:731.5,12:870,13:1020.5,14:1183,15:1357.5,16:1544,17:1742.5};
+var avgValue = {0:0,1:6.5,2:25,3:55.5,4:98,5:152.5,6:219,7:297.5,8:388,9:490.5,10:605,11:731.5,12:870,13:1020.5,14:1183,15:1357.5,16:1544,17:1742.5,18:1953,19:2175.5, 20:2410};
 
+// Grey out selected player
 $(document).on('click', '#player.btn-default', function () {
-    $(this).parent().children().removeClass('btn-default').addClass('chosen');  
+    if ($(this).siblings('#flag').hasClass('btn-danger'))
+        $(this).siblings('#flag').removeClass('btn-danger').addClass('prev-flagged');
+    else if ($(this).siblings('#yes').hasClass('btn-success')) {
+        $(this).siblings('#yes').removeClass('btn-success').addClass('prev-yes');
+    }
+    $(this).parent().children().removeClass('btn-default').addClass('chosen');
 });
 
+// Ungrey unselected player
 $(document).on('click', '#player.btn.chosen', function () {
+    if ($(this).siblings('#flag').hasClass('prev-flagged'))
+        $(this).siblings('#flag').removeClass('prev-flagged').addClass('btn-danger');
+    else if ($(this).siblings('#yes').hasClass('prev-yes'))
+        $(this).siblings('#yes').removeClass('prev-yes').addClass('btn-success');
     $(this).parent().children().removeClass('chosen').addClass('btn-default');  
 });
 
+// Flag and unflag a player
 $(document).on('click', '#flag.btn-default', function () {  
     $(this).toggleClass('btn-danger', 'btn-default');
     $(this).siblings('#player').toggleClass('btn-danger', 'btn-default');
 });
 
+// Select (draft) a player
 $(document).on('click', '#yes.btn-default', function () {
 	if ($(this).hasClass("mdl")) {
-		console.log("Ho");
-		$(this).removeClass('btn-default');
-	    $(this).addClass('btn-success');
+		$(this).toggleClass('btn-success', 'btn-default');
 	} else {
-		console.log("Hey");
-		$(this).removeClass('btn-default');
-	    $(this).addClass('btn-success');
-	   	picks++;
-	   	$('.picks').html(picks);
-	    curValue += $(this).data("rank");
-	    voa = avgValue[picks] - curValue;
-	    $('.voa').html(voa);
-	    updatePickTypes($(this).data("position"),1);
-	    $('.pickTypes').html('(RB: '+numRb+', WR: '+numWr+', TE: '+numTe+', QB: '+numQb+')');
+        if ($(this).siblings('#flag').hasClass('btn-danger')) {
+            $(this).siblings().removeClass('btn-danger').addClass('btn-success prev-flagged');
+        }
+        $(this).parent().children().removeClass('btn-default').addClass('btn-success');
+        updateScoreBoard($(this).data("rank"), $(this).data("position"), 1);
 	};
 }); 
 
+// Unselect a player
 $(document).on('click', '#yes.btn-success', function () {
 	if ($(this).hasClass('mdl')) {
-		$(this).removeClass('btn-success');
-	    $(this).addClass('btn-default');
+        $(this).toggleClass('btn-success', 'btn-default');
 	}  else {
-		$(this).removeClass('btn-success');
-	    $(this).addClass('btn-default');
-	    picks--;
-	    $('.picks').html(picks);
-	    curValue -= $(this).data("rank");
-	    voa = avgValue[picks] - curValue;
-	    $('.voa').html(voa);
-	    updatePickTypes($(this).data("position"),-1);
-	    $('.pickTypes').html('(RB: '+numRb+', WR: '+numWr+', TE: '+numTe+', QB: '+numQb+')');	    
+        if ($(this).siblings('#flag').hasClass('prev-flagged')) {
+            $(this).siblings().removeClass('btn-success prev-flagged').addClass('btn-danger');
+        }
+        $(this).parent().children().removeClass('btn-success').addClass('btn-default');
+        updateScoreBoard($(this).data("rank"), $(this).data("position"), -1);    
 	}
 }); 
 
+// Helper function to update the scoreboard, given a rank (#), position, and direction of change (1 or -1)
+function updateScoreBoard(rank, position, change) {
+    picks += 1 * change;
+    $('.picks').html(picks);
+    curValue += rank * change;
+    voa = avgValue[picks] - curValue;
+    $('.voa').html(voa);
+    updatePickTypes(position, 1 * change);
+    $('.pickTypes').html('(RB: '+numRb+', WR: '+numWr+', TE: '+numTe+', QB: '+numQb+')');
+}
+
+// Helper function to update picks given position and direction of change (1 or -1)
 function updatePickTypes(position, change) {
 	if (position == "RB") numRb += change;
 	else if (position == "WR") numWr += change;
